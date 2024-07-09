2284421dfb92469b6ecf47f6237b6a4177cb4138
officialsanchi <nwangwudinma22@gmail.com>
2024-07-09 15:47:46 +0100

    update

---
+++ classwork/HelloWorld.py
@@ -0,0 +1,31 @@
+name = input("Enter your name : ")
+age = input ("Enter your age : ")
+print(f' {name }g {age}')
+
+message = """your fullname is 
+chidinma and
+your age is 30"""
+print(message)
+
+
+name = input("Enter your name : ")
+
+if name:
+	print("your name is : " , name)
+	print('hello')
+	print('tech blazers')
+age =int ( input("Enter your name : "))
+
+if age >= 10:
+	print("you are eligible to make your own decisions" )
+	print('hello')
+	print('tech blazers')
+else:
+	print("you are a child")
+
+
+
+
+
+
+
---
+++ classwork/hello.py
@@ -0,0 +1,52 @@
+print("HELLO WORLD")
+
+n = 10
+m = 7 
+x = n+m
+print(x)
+
+g = 2**2
+print(g)
+
+
+
+print ("chidinma, how are you try and be pushing and taking your class, task and classwork seriously, it will help you to understand more concepts ");
+
+
+
+name = "chidinma"
+age = 30
+
+print(f'{name }  {age}')
+
+name = "sanchi"
+age = 20
+print(name)
+print(age)
+
+name = input()
+age = 35
+print (name , age)
+
+name = input("Enter your name : ")
+age = 35
+print (name , age)
+
+name = input("Enter your name : ")
+age = input ("Enter your age : ")
+age = int(age) + 10
+
+print (name ,age )
+
+
+password = input ("Enter password : ")
+number = len(password)
+print("*" * number)
+
+fristName = input("your full name")
+age = input ("your age is ")
+print = ( type (fristName), type (age))
+
+
+
+
